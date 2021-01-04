d3d4edd29d6ddedc088313ecb8ea81c5988c22ee
Andy <acameron851@gmail.com>
2021-01-04 11:51:15 +0000

    Added placeholder text to projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,21 +1,20 @@
+import './Projects.css';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import SideBar from '../SideBar';
 
 const Projects = () => {
 	return (
-		<div>
+		<div className='projects-container'>
 			<Grid container>
-				<Grid className='div-container' item xs={2} style={{ background: 'white' }}></Grid>
-				<Grid item xs={8} style={{ background: 'red' }}></Grid>
-				<Grid
-					item
-					xs={2}
-					direction='column'
-					justify='center'
-					alignItems='center'
-					style={{ background: 'blue' }}
-				>
+				<Grid className='div-container' item xs={2}></Grid>
+				<Grid item xs={8}>
+					<br />
+					<br />
+					<br />
+					Under Construction...
+				</Grid>
+				<Grid item xs={2} direction='column' justify='center' alignItems='center'>
 					<SideBar />
 				</Grid>
 			</Grid>
